e5dddf6f545c03851603ad5b4089c11515726ce4
nunjayi <epenyash@gmail.com>
2024-06-03 08:50:41 +0300

    solve the problems

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,49 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if(username == "admin" and password == "12345"):
+        return "Access granted"
+    elif(username == "ADMIN" and password == "12345"):
+        return "Access granted"
+    else:
+        return "Access denied"
+    
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if(temperature < 40):
+        response = "brisk"
+    elif(temperature >= 40 and temperature <= 65):
+        response = "a little chilly"
+    elif(temperature > 85):
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    return f"It's {response} out there!"
+    
 
 def fizzbuzz(num):
     # your code here
+    if(num % 3 == 0 and num % 5 == 0):
+        return "FizzBuzz"
+    elif(num % 3 == 0):
+        return "Fizz"
+    elif(num % 5 == 0):
+        return "Buzz"
+    else: 
+        return num
     pass
 
 def calculator(operation, num1, num2):
     # your code here
+    if(operation == "+"):
+        return num1 + num2
+    elif(operation == "-"):
+        return num1 - num2
+    elif(operation == "*"):
+        return num1 * num2
+    elif(operation == "/"):
+        return num1/num2
+    else:
+        print("Invalid operation!")
     pass
